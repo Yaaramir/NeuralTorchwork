cdcc68077a21389942bac7d4e674ff74a7348ee0
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-19 18:38:19 +0200

    Create dense layers and activation functions

--- neural_torchwork.py
+++ neural_torchwork.py
@@ -5,6 +5,14 @@ import torch
 # General settings
 nnfs.init()
 
+# Create model
+dense1 = torch.nn.Linear(in_features=2, out_features=512)
+activation1 = torch.nn.ReLU()
+dense2 = torch.nn.Linear(in_features=512, out_features=3)
+activation2 = torch.nn.Softmax()
+#loss = TODO
+#optimizer = TODO
+
 # Create dataset
 X_raw, y_raw = spiral_data(samples=100, classes=3)
 X = torch.tensor(X_raw)
